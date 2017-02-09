b056d2c4e3d1fb13607ba6fb93cd42f139ba7b7c
markusvhagen <markusvhagen@gmail.com>
2017-02-09 18:50:37 +0100

    Optimaliserte skriptet til å bli 10-12x ganger raskere
    Endret prosedyren som gjør om tuplen til en leselig streng. Fjernet unødvendige print-funksjoner som tar opp tid.

--- bruteforce.py
+++ bruteforce.py
@@ -1,27 +1,27 @@
 import time
 import itertools
 
-
 def bruteforce(passord):
+    print("Starter søk")
 
     # Importerer topp 500 passord-tekstfilen, samt deklarerer en tom liste.
     topp500_txt = open("top500.txt", "r")
     topp500 = []
-    
+
     # Legger de importerte passordene i en liste så de er lettere tilgjengelig.
     for linjer in topp500_txt:
         topp500.append(linjer.strip())
 
-    # Starter tiden.
+    # Starter tiden, ved å finne ut hva tiden er nå
     start_tid = time.time()
 
     # Sjekker om passord er i listen av de 500 mest brukte.
     for kombinasjon in topp500:
-        print("Sjekker " + kombinasjon)
         if kombinasjon == passord:
             tid_brukt = str(time.time() - start_tid)
             print("Fant passord!" + " Passord: " + kombinasjon + " Brukte tiden: " + tid_brukt + " sekunder")
-            break
+            time.sleep(5)
+            raise SystemExit
 
     # Hvis koden fortsetter her er ikke passordet i topp 500
     # Sjekker da alle andre mulige kombinasjoner ved en bruteforce, maks tegn er satt til 8
@@ -33,37 +33,24 @@ def bruteforce(passord):
 
         # Definerer testmengden
         testMengde = (itertools.product(alleKombinasjoner, repeat = lengde))
-        print("\n \n")
 
-        print("Sjekker nå mulige kombinasjoner med " + str(lengde), " tegn")
-        print("\n")
-        print("ANTALL FORSØK PER SEKUND: " + str((teller / (time.time() - start_tid))))
-        print("ANTALL SEKUNDER SIDEN START: " + str((time.time() - start_tid)))
-        print("ANTALL ULIKE KOMBINASJONER PRØVD HITTIL: " + str(teller))
-              
-        
         for i in testMengde:
 
-            # Konverterer tuple til string
-            i = str(i)
-
-            # Gjør strengen ren
-            i = i.replace("[", "")
-            i = i.replace("]", "")
-            i = i.replace("'", "")
-            i = i.replace(" ", "")
-            i = i.replace(",", "")
-            i = i.replace("(", "")
-            i = i.replace(")", "")
-
             teller += 1
+            # Konverterer tuple til string
+            i = "".join(i)
 
             if i == passord:
                 tid_brukt = str(time.time() - start_tid)
-                print("Fant passord!" + " Passord: " + forsøk + " Brukte tiden: " + tid_brukt + " sekunder")
-                break
+                passordsek = str(teller / (time.time() - start_tid))
+                print("Fant passord!" + " Passord: " + i + " Brukte tiden: " + tid_brukt + " sekunder")
+                filnavn = "resultat/" + passord + ".txt"
+                resultat = open(filnavn, "w")
+                resultat.write("Strengen " + passord + " ble funnet på " + tid_brukt + " sekunder, og etter " + str(teller) + " forsøk \n \n")
+                resultat.write("I gjennomsnitt ble det undersøkt " + passordsek + " strenger per sekund.")
+                time.sleep(5)
+                raise SystemExit
+
 
 faktisk_passord = input("Skriv inn passord for å sjekke hvor lang tid det ville tatt å bruteforce det: ")
 bruteforce(faktisk_passord)
-
-
